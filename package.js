fc6c0ad7976c71c7f4e1bfb1540d8f71b141b896
Gabriel Engel <gabriel.engel@fgsys.com>
2014-06-30 15:28:34 -0300

    Initial Release

--- package.js
+++ package.js
@@ -1,11 +1,13 @@
 Package.describe({
-	summary: "Fast, light, mobile-friendly and responsive lightbox and modal dialog plugin. Open inline HTML, ajax loaded content, image, form, iframe (YouTube video, Vimeo, Google Maps), photo gallery. Animation effects added with CSS3 transitions. For meteor."
+    summary: "Fast, light, mobile-friendly and responsive lightbox and modal dialog plugin. Open inline HTML, ajax loaded content, image, form, iframe (YouTube video, Vimeo, Google Maps), photo gallery. Animation effects added with CSS3 transitions. For meteor."
 });
 
-Package.on_use(function (api) {
-	api.use('jquery', 'client');
-	api.add_files([
-		'vendor/dist/magnific-popup.css',
-		'vendor/dist/jquery.magnific-popup.js'
-	], 'client');
+Package.on_use(function(api) {
+    api.use([
+    	'jquery'
+    ], ['client']);
+    api.add_files([
+        'vendor/dist/magnific-popup.css',
+        'vendor/dist/jquery.magnific-popup.js'
+    ], ['client']);
 });
